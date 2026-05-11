a921f2d780288d249372cac609d5ef6218deb613
RAEHAN-SP <hanz.26r@gmail.com>
2026-05-12 00:10:00 +0800

    Update jadibot.js

--- src/jadibot.js
+++ src/jadibot.js
@@ -138,10 +138,12 @@ async function JadiBot(conn, from, m, RAEHANDATA) {
 ┃Tinggalkan Pesan
 ┃
 ▬▭▬▭▬▭▬▭▬▬▭▬▭
-┃
-┃ɪɴɪ ᴀᴅᴀʟᴀʜ ᴋᴇᴄᴇʀᴅᴀsᴀɴ ʙᴜᴀᴛᴀɴ
-┃ᴅɪ ʙᴜᴀᴛ ᴏʟᴇʜ ʀᴀᴇʜᴀɴ
-┃
+
+FOLLOW INSTAGRAM 
+CREATOR 👇👇👇
+
+https://www.instagram.com/hanz_932?igsh=Ymp6dTNjYzhtODFq
+
 ▬▭▬▭▬▭▬▭▬▬▭▬▭`
 					let msg = await client[from].sendMessage(id.from, { image: {url: 'https://ar-hosting.pages.dev/1775121854710.png'},caption: hanzzz, mentions: [id.from]});
 							
