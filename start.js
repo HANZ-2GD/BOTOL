dad8b21483a3f4aa487f5255ab2950c49d3d3dcb
RAEHAN-SP <hanz.26r@gmail.com>
2026-05-16 18:22:17 +0800

    Update start.js

--- start.js
+++ start.js
@@ -7,7 +7,7 @@ import { watchFile, unwatchFile } from 'fs'
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 function start() {
-	let args = [path.join(__dirname, 'index.js'), ...process.argv.slice(2)]
+	let args = [path.join(__dirname, 'RaehanStart.js'), ...process.argv.slice(2)]
 	let p = spawn(process.argv[0], args, {
 		stdio: ['inherit', 'inherit', 'inherit', 'ipc']
 	}).on('message', data => {
